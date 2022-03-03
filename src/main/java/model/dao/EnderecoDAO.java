f45e00afa0d2b17e647def30d6e32c8bf571b0af
IVO RAMOS JUNIOR <ivo.junior@alunos.sc.senac.br>
2022-03-03 11:04:38 -0300

    Criação da classe EnderecoDAO.java e estruturação

---
+++ src/main/java/model/dao/EnderecoDAO.java
@@ -0,0 +1,49 @@
+package model.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entity.Endereco;
+
+public class EnderecoDAO {
+	
+	//inserir
+	public Endereco inserir(Endereco novoEndereco) {
+		//TODO implementar
+		
+		return novoEndereco;
+	}
+	
+	//atualizar
+	public boolean atualizar(Endereco endereco) {
+		boolean atualizou = false;
+		//TODO implementar
+		
+		return atualizou;
+	}
+	
+	//remover
+	public boolean remover(int id) {
+		boolean removeu = false;
+		//TODO implementar
+		
+		return removeu;
+	}
+	
+	//consultar por id
+	public Endereco consultar(int id) {
+		Endereco enderecoConsultado = null;
+		//TODO implementar
+		
+		return enderecoConsultado;
+	}
+	
+	// consultar todos
+	public List<Endereco> consultarTodos(){
+		List<Endereco> enderecosConsultados = new ArrayList<Endereco>();
+		//TODO implementar
+		
+		return enderecosConsultados;
+	}
+
+}
